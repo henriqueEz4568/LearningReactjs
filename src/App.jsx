25940c7f32d5270dec017e5c43c5c8c267e1b92f
henriqueEz4568 <rick.scioli@gmail.com>
2024-02-09 19:04:08 -0300

    quase fazendo merda

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,36 @@ import { Sidebar } from './components/Sidebar'
 import { Header } from './components/Header'
 import { Post } from './post'
 import styles from './App.module.css'
-
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00'),
+  },
+];
 function App() {
  
 
@@ -14,7 +43,7 @@ function App() {
         <Sidebar />
         <main>
           <Post
-            author="Diego Fernandes"
+            author="Diegoss FernaSSndes"
             content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
           />
           <Post
--- src/post.jsx
+++ src/post.jsx
@@ -8,3 +8,6 @@ export function Post(props){
     </p></div>
     )
 }
+
+
+	
