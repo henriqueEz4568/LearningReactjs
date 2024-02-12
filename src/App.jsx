27dafebdca579ca213f366c19f67c1ed18ba2726
henriqueEz4568 <rick.scioli@gmail.com>
2024-02-12 01:38:35 -0300

    header ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,62 +1,17 @@
 import { useState } from 'react'
-
-import { Sidebar } from './components/Sidebar'
 import { Header } from './components/Header'
-import { Post } from './post'
-import styles from './App.module.css'
-const posts = [
-  {
-    id: 1,
-    author: {
-      avatarUrl: 'https://github.com/diego3g.png',
-      name: 'Diego Fernandes',
-      role: 'CTO @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-05-03 20:00:00'),
-  },
-  {
-    id: 2,
-    author: {
-      avatarUrl: 'https://github.com/maykbrito.png',
-      name: 'Mayk Brito',
-      role: 'Educator @Rocketseat'
-    },
-    content: [
-      { type: 'paragraph', content: 'Fala galera 👋' },
-      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
-      { type: 'link', content: 'jane.design/doctorcare' },
-    ],
-    publishedAt: new Date('2022-05-10 20:00:00'),
-  },
-];
+/*import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'*/
+
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (<div>
- <Header />
- <div className={styles.wrapper}>
-        <Sidebar />
-        <main>
-          <Post
-            author="Diegoss FernaSSndes"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
-          />
-          <Post
-            author="Gabriel Buzzi"
-            content="Um novo post muito legal"
-          />
-        </main>
-      </div>
-  
-  
-  
- </div>  
-  )
+   
+   <Header />
+   <p>hello world</p>   
+   </div>)
 }
 
 export default App
